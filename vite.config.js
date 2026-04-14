d0bd452f36e4a07bfbdc1a2368c157533cd7ef8d
Evaldo Lopes <evaldolbj@gmail.com>
2026-04-14 18:24:44 -0300

    Laravel 13

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,27 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravel, { refreshPaths } from "laravel-vite-plugin";
 import tailwindcss from '@tailwindcss/vite';
+
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            // refresh: true,
+            refresh: [
+                {
+                    paths: ["resources/views/**"],
+                    config: { delay: 300 },
+                },
+            ],
         }),
         tailwindcss(),
     ],
     server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
+        host: "0.0.0.0",
+        port: 5174,
+        hmr: {
+            host: "pwpe.local",
         },
     },
 });
